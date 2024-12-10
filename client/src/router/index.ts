5d95047871f1eaebb3125be3117a0781d67298b8
lakkinzimusic <lakkinzimusic@gmail.com>
2024-12-10 17:00:48 +0300

    MainPage

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
+const MainPage = () => import('@/components/MainPage.vue');
 import indexAdminRoutes from '@/router/indexAdminRoutes';
 
 const routes: Array<RouteRecordRaw> = [...indexAdminRoutes];
+
+routes.push({
+  path: '',
+  name: 'MainPage',
+  component: MainPage,
+});
 
 const router = createRouter({
   history: createWebHistory(''),
