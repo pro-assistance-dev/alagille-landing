22bc4e2562f4a340664fe10de001db68055809b3
lakkinzimusic <lakkinzimusic@gmail.com>
2024-12-19 15:26:37 +0300

    LostMig

---
+++ server/migrations/20241219105028_add_fields.up.sql
@@ -0,0 +1,9 @@
+ALTER TABLE patients
+  ADD COLUMN is_russion bool, 
+  add column region varchar,
+  add column city varchar,
+  add column diagnosis varchar,
+  add column drug varchar,
+  add column ill_history_id uuid,
+  add column accept_id uuid
+;
